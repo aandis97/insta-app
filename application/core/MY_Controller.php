358870ffbb90df222f344953a65884bc96f8f720
aandis97 <achandis97@gmail.com>
2022-12-18 18:45:16 +0700

    rafactor: move hmvc to thrid party, fix env mission config

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -5,5 +5,45 @@ class MY_Controller extends MX_Controller
     public function __construct()
     {
         parent::__construct();
+        $this->hmvc();
+    }
+
+    /**
+     * HMVC : fix callback form_validation
+     * https://bitbucket.org/wiredesignz/codeigniter-modular-extensions-hmvc.
+     *
+     * @return void
+     */
+    private function hmvc()
+    {
+        $this->load->library('form_validation');
+        $this->form_validation->CI = &$this;
+    }
+
+    /**
+     * Render view
+     *
+     * @param  string $view
+     * @param  array $vars
+     */
+    protected function render($view, $vars = [])
+    {
+        return $this->twig->display($view, $vars);
+    }
+
+    /**
+     * Return response with json format
+     * 
+     * @param mixed $payload
+     * @param int $statusCode
+     */
+    protected function jsonResponse($payload, int $statusCode = 200)
+    {
+        $this->output
+            ->set_status_header($statusCode)
+            ->set_content_type('application/json', 'utf-8')
+            ->set_output(json_encode($payload, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES))
+            ->_display();
+        exit;
     }
 }
---
+++ application/hooks/Environment.php
@@ -0,0 +1,11 @@
+<?php
+class Environment
+{
+    public function init()
+    {
+        $dotenv = Dotenv\Dotenv::createUnsafeMutable(FCPATH . "../");
+        $dotenv->load();
+
+        $dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS']);
+    }
+}
